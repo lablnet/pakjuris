297ecb8803c94226fdd649806049e5b7ad18171a
Muhammad Umer Farooq <umer@lablnet.com>
2025-04-06 08:37:24 +0500

    Add date parsing utility function
    - Introduced a new utility function `parseDateString` in `timeutils.ts` to convert date strings into a formatted date representation.
    - The function handles optional date strings and returns a user-friendly format, enhancing date handling capabilities in the application.
    
    These changes improve the utility functions available for date manipulation, contributing to better data presentation and user experience.

---
+++ web/src/utils/timeutils.ts
@@ -0,0 +1,22 @@
+
+function parseDateString(dateString?: string): string {
+    if (!dateString) {
+        return '';
+    }
+    const date = new Date(dateString);
+
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        // hour: '2-digit',
+        // minute: '2-digit',
+        // second: '2-digit',
+        // hour12: false
+    };
+
+    return date.toLocaleString('en-US', options);
+}
+
+export { parseDateString };
+
