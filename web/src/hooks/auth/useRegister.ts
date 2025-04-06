fee5c14fa2ff2ad7474c88ca306de002cca35c3c
Muhammad Umer Farooq <umer@lablnet.com>
2025-04-06 18:49:24 +0500

    Add registration hook for user authentication
    - Introduced a new `useRegister` hook to manage user registration, including input validation and error handling.
    - Implemented functions for registering a user, resending OTP, and validating OTP, enhancing the registration process.
    - Integrated Zustand for state management of registration inputs and loading states, improving user experience during the registration flow.
    
    These changes provide a comprehensive solution for user registration, ensuring a smoother onboarding experience and better error management.

---
+++ web/src/hooks/auth/useRegister.ts
@@ -0,0 +1,147 @@
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
+import { useUserStore } from '../../stores/userStore';
+import { parseErrors } from '../../utils/error';
+import { useToast } from '../../components/ToastComp';
+import useOTP from './useOTP';
+import { useRegisterStore } from '../../stores/registerStore';
+import { MeType } from '../../types/api';
+
+const useRegister = () => {
+  const {
+    loading,
+    setLoading,
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    passwordConfirm,
+    setPasswordConfirm,
+    step,
+    setStep,
+    agreeToTerms,
+  } = useRegisterStore();
+
+  const navigate = useNavigate();
+  const { setUser } = useUserStore();
+  const toast = useToast();
+
+
+  const register = async () => {
+    // validate inputs
+    if (!email || !password || !passwordConfirm) {
+      return toast({
+        type: 'error',
+        message: 'All fields are required',
+      });
+    }
+    // agrees to terms.
+    if (!agreeToTerms) {
+      return toast({
+        type: 'error',
+        message: 'Please agree to terms and conditions',
+      });
+    }
+    try {
+      setLoading(true);
+      await api.auth.register.signup({
+        full_name: `${firstName} ${lastName}`,
+        email,
+        password,
+      });
+      setStep(2);
+    } catch (error: any) {
+      let err = parseErrors(error.response.data)
+      console.log ("Error occured! ", err)
+      err.general && toast({
+        type:'error',
+        message: err.general,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const resendOTP = async () => {
+    try {
+      setLoading(true);
+      await api.auth.register.resendOTP({ email });
+      toast({type: "info", message: "OTP resent successfully" });
+    } catch (error: any) {
+        let err = parseErrors(error.response.data)
+        console.log ("Error occured! ", err)
+        err.general && toast({
+          type:'error',
+          message: err.general,
+        });
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const validateOTP = async (otp: string) => {
+    try {
+      setLoading(true);
+      const response: MeType = await api.auth.register.validateOTP({ otp, email });
+      setUser(response.user);
+      toast({type: "success", message: "OTP verified successfully, redirected to account setup" });
+      navigate('/account-setup');
+    } catch (error: any) {
+      let err = parseErrors(error.response.data)
+        console.log ("Error occured! ", err)
+        err.general && toast({
+          type:'error',
+          message: err.general,
+        });
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const { code, setCode, resendCountdown, handleResendCode } = useOTP(
+    resendOTP,
+    validateOTP,
+    loading,
+    setLoading
+  );
+
+  const next = async (): Promise<void> => {
+    if (step === 1) {
+      await register();
+    }
+  }
+
+  const prev = (): void => {}
+
+  return {
+    register,
+    resendOTP,
+    validateOTP,
+    loading,
+    setLoading,
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    passwordConfirm,
+    setPasswordConfirm,
+    step,
+    setStep,
+    next,
+    prev,
+    code,
+    setCode,
+    handleResendCode,
+    resendCountdown
+  };
+};
+
+export default useRegister;
