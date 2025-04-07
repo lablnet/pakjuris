2625f1326b7432545b932979e912fdadd54702d7
Muhammad Umer Farooq <umer@lablnet.com>
2025-04-07 12:19:58 +0500

    Update index.ts to export new dialog components
    - Added exports for ConfirmationDialog and EditDialog components in the index.ts file, enhancing the UI component library.
    - This update supports ongoing efforts to improve user interactions and provide more options for modal interfaces within the application.

--- web/src/components/ui/index.ts
+++ web/src/components/ui/index.ts
@@ -2,3 +2,5 @@ export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { ToastManager, useToast } from './ToastComp';
 export { default as Dialog } from './Dialog';
+export { default as ConfirmationDialog } from './ConfirmationDialog';
+export { default as EditDialog } from './EditDialog';
